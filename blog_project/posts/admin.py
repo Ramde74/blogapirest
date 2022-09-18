92489eb88b4fc087ef39bf1b79f0113d21a9dd8d
9998236414 <karavadararamde74@gmail.com>
2022-09-18 10:47:10 +0530

    user authentificated rest api project

---
+++ blog_project/posts/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import Post
+admin.site.register(Post)
+
+
+# blo
+#password   blo123
+
+# testuser
+# password  main5467
+
+# user authentificated project
